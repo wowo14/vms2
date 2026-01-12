e8602bd06ee42cddfd62680638122e331aeaa8af
Anang Fahmi Syarif <amikaja@gmail.com>
2026-01-12 11:41:35 +0700

    link di paket download template

--- views/paketpengadaan/index.php
+++ views/paketpengadaan/index.php
@@ -97,7 +97,7 @@ AppAsset::register($this);
                             'data-original-title' => Yii::t('yii2-ajaxcrud', 'Kirim DPP'),
                         ]
                     ),
-                ]) : ''),
+                ]) : Url::a('<i class="fa fa-file"></i>&nbsp; ' . Yii::t('yii2-ajaxcrud', 'File Template Pengadaan Terbaru'), ['site/downloads'], ['class' => 'btn btn-primary btn-xs']) ),
                 'after' => BulkButtonWidget::widget([
                     'buttons' => Html::a(
                         '<i class="fa fa-trash"></i>&nbsp; ' . Yii::t('yii2-ajaxcrud', 'Delete All'),
